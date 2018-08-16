a61938ff9cf6f0f86025fead8396e86f9c0c0da2
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2018-08-17 00:00:26 +1000

    Delete read.js

--- a3/read.js
+++
@@ -1,6 +0,0 @@
-var fs =require('fs');
-fs.readFile(__dirname + '/' + filename, 'utf8', fucntion(err, data){
- 			//stats[i] = stat;
- 			console.log('');
- 			console.log('\033[90m'+ data.replace(/ ( .*)/g,'  $1')+'\033[39m');
-		});
